53ed503e2744ef800fbee5f3ddf59eb30b06affb
Nontiwat-Nonkhamchan <nontiwat.n@kkumail.com>
2025-03-10 23:59:30 +0700

    Update pidgey.cs

--- pidgey.cs
+++ pidgey.cs
@@ -7,9 +7,9 @@ using PokemondexN.pokemondex3;
 
 namespace PokemondexN
 {
-    public class Jigglypuff : Pokemon
+    public class pidgey : Pokemon
     {
-        public Jigglypuff() : base("Jigglypuff", "NorMal/Fairy", 0.5, 5.5, 115, 45, 20, 45, 25, 20)
+        public pidgey() : base("Jigglypuff", "NorMal/Fairy", 0.5, 5.5, 115, 45, 20, 45, 25, 20)
         {
         }
     }
